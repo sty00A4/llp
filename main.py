e5e65c964851b1e309d9247f5a5d328895a2d991
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-08-08 01:06:53 +0200

    rename

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import src.lexer as lexer
 
-with open("tests/test.lm", "r") as f:
-    tokens = lexer.tokenize("tests/test.lm", f.read())
+with open("tests/test.llp", "r") as f:
+    tokens = lexer.tokenize("tests/test.llp", f.read())
     for tok in tokens: print(tok, end=" ")
     print()
