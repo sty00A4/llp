6e80a7904eee1fce32b880382c3602114933e9b8
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-08-08 01:09:34 +0200

    token view

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import src.lexer as lexer
 
 with open("tests/test.llp", "r") as f:
     tokens = lexer.tokenize("tests/test.llp", f.read())
-    for tok in tokens: print(tok, end=" ")
+    for tok in tokens: print(tok)
     print()
